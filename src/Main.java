332c2c98608e9811fdbbcde06ca1d1f2b5277b09
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-05-15 13:21:12 +0800

    Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        PraktekFrame praktekFrame = new PraktekFrame();
+        praktekFrame.meletakkanKomponen();
+        praktekFrame.setVisible(true);
+        praktekFrame.menambhakanListener();
+        praktekFrame.getGroupRadio();
+        praktekFrame.aTampil.setEnabled(false);
+    }
+
+}
